5c67a1708e7f3aa375156fd918d851762279fc62
Prince <princekumar7320918928@gmail.com>
2024-09-15 23:14:31 +0530

    upd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1>
+      <h1 className='text-3xl font-bold underline my-4'>
         Hello Vite + React
       </h1>
-      <h2>
+      <h2 className='text-2xl'>
         create your own branch and start adding to it
       </h2>
     </>
