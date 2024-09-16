393167b42b78b73f48ced01ca4da8cb2f2ac3023
Prince <princekumar7320918928@gmail.com>
2024-09-16 15:36:34 +0530

    upd

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,11 @@ function Home() {
       <h1 className='text-3xl font-bold underline my-4'>
         Hello Vite + React <br />create your own branch and start adding to it
       </h1>
+      <ul>
+        <li>router has been added</li>
+        <li>redux-toolkit has been added</li>
+        <li>tailwind css has been added</li>
+      </ul>
     </div>
   )
 }
