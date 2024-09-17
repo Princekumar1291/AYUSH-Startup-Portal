a665a4405c23606da346c473a635f2a336a3ebd4
shubhamgit <git@gmail.com>
2024-09-17 14:40:46 +0530

    comit

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const path = require("path");
 const compression = require("compression");
 const { ENV } = require("./config/env");
-const authRoutes = require("./routes/auth.route");
+const authRoutes = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
 app.use(compression());
 app.use(cors());
 
-app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/", authRoutes);
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running in port ${ENV.PORT}`);
