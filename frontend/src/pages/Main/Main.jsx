6e6b6487ed583b82e6d1684c5d83964b4452c15f
Prince <princekumar7320918928@gmail.com>
2024-09-17 16:17:21 +0530

    added footer

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Carousel from '../../components/Carousel/Carousel'
+import Footer from '@/components/Footer/Footer'
 function Main() {
   return (
     <div className='w-full'>
       <Navbar/>
-      <Carousel />
+      <Footer></Footer>
     </div>
   )
 }
