44a4d44b275720e914ed14a08c6a4e91537af793
Bishal Moktan <bishalmoktan270@gmail.com>
2024-09-17 12:50:15 +0530

    feat: Configured backend for register and login

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-var compression = require('compression')
-
-const route = require("./routes/routes");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const compression = require("compression");
+const { ENV } = require("./config/env");
+const authRoutes = require("./routes/auth.route");
 
 const app = express();
-app.use(compression())
+app.use(express.json());
+app.use(compression());
 app.use(cors());
 
-app.use('/api/v1',route);
+app.use("/api/v1/auth", authRoutes);
 
-app.listen(3000,()=>{
-    console.log("Server started at 3000");
-})
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running in port ${ENV.PORT}`);
+});
