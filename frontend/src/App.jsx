a513423a629751dbbdeeebfd9cbdf0457052daef
Prince <princekumar7320918928@gmail.com>
2024-09-17 15:48:13 +0530

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import Dashboard from './pages/Dashboard/Dashboard'
 import ApplicationForm from './pages/ApplicationForm/ApplicationForm'
 import DocumentUpload from './pages/DocumentUpload/DocumentUpload'
 import HelpCenter from './pages/FAQ/FAQ'
+import Main from './pages/Main/Main'
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/application-form" element={<ApplicationForm />} />
         <Route path="/document-upload" element={<DocumentUpload />} />
---
+++ frontend/src/pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Carousel from '../../components/Carousel/Carousel'
+function Main() {
+  return (
+    <div className='w-full'>
+      <Navbar/>
+      <Carousel />
+    </div>
+  )
+}
+
+export default Main
